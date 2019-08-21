1faf0a258ced2f273698b45d6f4cad9e05de1128
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-08-21 15:44:32 +0200

    Add active class to product card buttons

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
 console.log('hello');
+
+let buttonsOutline = document.querySelectorAll('.btn-outline');
+
+function classToggle () {
+  this.classList.toggle('active');
+}
+
+buttonsOutline.forEach(function (el) {
+  el.addEventListener('click', classToggle);
+});
