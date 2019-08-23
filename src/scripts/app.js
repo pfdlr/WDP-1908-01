5b55a5f13a8dca6e720b5aa74d9e61ffa35dee90
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-08-23 13:56:04 +0200

    Modify .cart-counter in _header.scss & removed js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,1 @@
 console.log('hello');
-
-let cartCount = document.querySelector('.cart-counter');
-
-cartCount.addEventListener('DOMSubtreeModified', function () {
-  let value = cartCount.innerText.length;
-  if (value > 2) {
-    cartCount.classList.add('smaller');
-  }
-  if (value < 3) {
-    cartCount.classList.remove('smaller');
-  }
-});
