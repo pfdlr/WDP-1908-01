4fdea91360290e643492ebc6ec20d493e3120631
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-08-23 13:01:23 +0200

    Add responsive header

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,12 @@
 console.log('hello');
+
+const toggleBtn = document.querySelector('.navbar-toggler');
+const menu = document.querySelector('#menu');
+
+if (window.screen.width < 768) {
+  menu.classList.remove('display');
+}
+
+toggleBtn.addEventListener('onclick', function () {
+  menu.classList.toggle('display');
+});
