5b23d13a1388ab8c12c7df7c022df301e1af02ad
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-08-26 19:01:31 +0200

    Add responsive styles for top-bar and company-claim

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,13 @@
 console.log('hello');
 
 let buttonsOutline = document.querySelectorAll('.btn-outline');
+let loginText = document.querySelectorAll('.login');
+
+if (window.screen.width < 768) {
+  loginText.forEach(function (el) {
+    el.classList.add('hidden');
+  });
+}
 
 function classToggle (e) {
   e.preventDefault();
