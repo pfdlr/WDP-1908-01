90a63e1f7a384ed87e7aa1318fb9ce061deea3f8
Piotr Fidler <fidler.piotr@gmail.com>
2019-08-26 22:15:26 +0200

    add carousel- at work

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+/* New furniture - carousel */
+/* global tns */
+tns({
+  container: '.product-carousel',
+  items: 1,
+  mouseDrag: true,
+  responsive: {
+    575: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
