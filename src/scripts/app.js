f80848a94dfd535abe8783e2ff5a40134ce74148
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-08-31 22:14:12 +0200

    Add brands slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-/* New furniture - carousel WDP190801-18*/
+/* New furniture - carousel WDP190801-18 */
 /* global tns */
 tns({
   container: '.product-carousel',
@@ -19,64 +19,85 @@ tns({
   }
 });
 
+tns({
+  container: '.brands-slider',
+  items: 1,
+  mouseDrag: true,
+  slideBy: 'page',
+  edgePadding: 23,
+  prevButton: '#brands-prev',
+  nextButton: '#brands-next',
+  nav: false,
+  responsive: {
+    575: {
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 6
+    }
+  }
+});
+
 /*
     This script replaceing default select style - hide select and replace with ul li list for styling new list
 */
 
-// set select category element to variable
-let selectEle = document.getElementById('category');
+// // set select category element to variable
+// let selectEle = document.getElementById('category');
 
-let numberOfOptions = selectEle.children.length; // Get number of options item
-selectEle.classList.add('select-hidden'); // add new class to category with id = category
+// let numberOfOptions = selectEle.children.length; // Get number of options item
+// selectEle.classList.add('select-hidden'); // add new class to category with id = category
 
-// wrap old element with new div.select-styled
-let orgHtml = selectEle.outerHTML;
-let newselect = "<div class='select'>" + orgHtml + '</div>';
-selectEle.outerHTML = newselect;
+// // wrap old element with new div.select-styled
+// let orgHtml = selectEle.outerHTML;
+// let newselect = "<div class='select'>" + orgHtml + '</div>';
+// selectEle.outerHTML = newselect;
 
-// create new element and append to new wraped div at the end
-document.getElementsByClassName('select')[0].innerHTML +=
-  "<div class='select-styled'></div>";
-let styledSelect = selectEle.firstElementChild.innerHTML;
+// // create new element and append to new wraped div at the end
+// document.getElementsByClassName('select')[0].innerHTML +=
+//   "<div class='select-styled'></div>";
+// let styledSelect = selectEle.firstElementChild.innerHTML;
 
-document.getElementsByClassName('select-styled')[0].innerHTML = styledSelect;
+// document.getElementsByClassName('select-styled')[0].innerHTML = styledSelect;
 
-// create new element ul and add new class
-let newElem3 = document.createElement('ul');
-newElem3.classList.add('select-options');
-document.getElementsByClassName('select-styled')[0].outerHTML += newElem3.outerHTML;
+// // create new element ul and add new class
+// let newElem3 = document.createElement('ul');
+// newElem3.classList.add('select-options');
+// document.getElementsByClassName('select-styled')[0].outerHTML += newElem3.outerHTML;
 
-// variable with query for new elements
-let newOpt = document.querySelectorAll('.category select option');
-let soption = document.getElementsByClassName('select-options')[0];
+// // variable with query for new elements
+// let newOpt = document.querySelectorAll('.category select option');
+// let soption = document.getElementsByClassName('select-options')[0];
 
-// for each options element add to new li item
-for (var i = 0; i < numberOfOptions; i++) {
-  soption.innerHTML +=
-    '<li rel=' + newOpt[i].getAttribute('value') + '>' + newOpt[i].innerHTML + '</li>';
-}
-// at begining set display to none
-soption.style.display = 'none';
+// // for each options element add to new li item
+// for (var i = 0; i < numberOfOptions; i++) {
+//   soption.innerHTML +=
+//     '<li rel=' + newOpt[i].getAttribute('value') + '>' + newOpt[i].innerHTML + '</li>';
+// }
+// // at begining set display to none
+// soption.style.display = 'none';
 
-// if clicked on button hide/show active list
-document
-  .getElementsByClassName('select-styled')[0]
-  .addEventListener('click', function (e) {
-    e.stopPropagation();
-    e.target.classList.toggle('active');
-    // set item to show or not
-    if (soption.style.display === 'none') {
-      soption.style.display = 'block';
-    } else {
-      soption.style.display = 'none';
-    }
-  });
-// if clicked button else where add/remove active class
-document.addEventListener('click', function () {
-  document.getElementsByClassName('select-styled')[0].classList.remove('active');
-  soption.style.display = 'none';
-  });
-
+// // if clicked on button hide/show active list
+// document
+//   .getElementsByClassName('select-styled')[0]
+//   .addEventListener('click', function (e) {
+//     e.stopPropagation();
+//     e.target.classList.toggle('active');
+//     // set item to show or not
+//     if (soption.style.display === 'none') {
+//       soption.style.display = 'block';
+//     } else {
+//       soption.style.display = 'none';
+//     }
+//   });
+// // if clicked button else where add/remove active class
+// document.addEventListener('click', function () {
+//   document.getElementsByClassName('select-styled')[0].classList.remove('active');
+//   soption.style.display = 'none';
+//   });
 
 const toggleBtn = document.querySelector('.navbar-toggler');
 const overlay = document.querySelector('.overlay');
@@ -84,7 +105,7 @@ const overlay = document.querySelector('.overlay');
 toggleBtn.addEventListener('click', function () {
   overlay.classList.toggle('hidden');
 });
-  
+
 let buttonsOutline = document.querySelectorAll('.btn-outline');
 
 function classToggle (e) {
