9b778f1d313fccab8c108b390f95985803debf81
Monika Zajac <mnk.zajac@gmail.com>
2019-10-13 16:28:34 +0200

    Add star review feature

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -127,7 +127,6 @@ buttonsOutline.forEach(function (el) {
   el.addEventListener('click', classToggle);
 });
 
-
 /*
     This script change tabs with fade effect
 */
@@ -165,4 +164,54 @@ function showTab (tab) {
       }, 300);
     });
   });
+}
+
+// Stars Review - WDP190801-19
+
+var stars = document.querySelectorAll('.stars a');
+var starsWrapper = document.querySelectorAll('.stars');
+
+for (let star of stars) {
+  star.addEventListener('click', onStarClick);
+  star.addEventListener('mouseenter', onStarEnter);
+}
+
+for (let wrapper of starsWrapper) {
+  wrapper.addEventListener('mouseleave', onStarLeave);
+}
+
+function onStarClick (event) {
+  updateStars(event.currentTarget, 'active');
+}
+
+function onStarEnter (event) {
+  updateStars(event.currentTarget, 'hovered');
+}
+
+function onStarLeave (event) {
+  var allStars = event.currentTarget.querySelectorAll('.stars a');
+  var allStarsArr = Array.from(allStars);
+  var i = 0;
+
+  for (i = 0; i < allStarsArr.length; i++) {
+    allStarsArr[i].classList.remove('hovered');
+  }
+}
+
+function updateStars (element, className) {
+  var parent = element.parentNode;
+  var allStars = parent.querySelectorAll('.stars a');
+  var allStarsArr = Array.prototype.slice.call(allStars);
+  var currentElementPosition = allStarsArr.indexOf(element);
+  var i = 0;
+
+  for (i = 0; i < allStarsArr.length; i++) {
+    allStarsArr[i].classList.remove('full');
+
+    if (i <= currentElementPosition) {
+      allStarsArr[i].classList.add(className);
+    } else {
+      allStarsArr[i].classList.remove(className);
+    }
+  }
 }
