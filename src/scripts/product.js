6572695b190b56cb2c45252b3ea4fbd737b07e4b
Monika Zajac <mnk.zajac@gmail.com>
2019-10-14 22:08:33 +0200

    Update html & styles for tabs

--- src/scripts/product.js
+++ src/scripts/product.js
@@ -17,3 +17,55 @@ if (window.screen.width < 768) {
     el.classList.add('hidden');
   });
 }
+
+// stars review
+
+var stars = document.querySelectorAll('.stars a');
+var starsWrapper = document.querySelectorAll('.stars');
+
+for (let star of stars) {
+  star.addEventListener('click', onStarClick);
+  star.addEventListener('mouseenter', onStarEnter);
+}
+
+for (let wrapper of starsWrapper) {
+  wrapper.addEventListener('mouseleave', onStarLeave);
+}
+
+function onStarClick (event) {
+  updateStars(event.currentTarget, 'active');
+}
+
+function onStarEnter (event) {
+  updateStars(event.currentTarget, 'hovered');
+}
+
+function onStarLeave (event) {
+  var allStars = event.currentTarget.querySelectorAll('.stars a');
+  var allStarsArr = Array.from(allStars);
+  var i = 0;
+
+  for (i = 0; i < allStarsArr.length; i++) {
+    allStarsArr[i].classList.remove('hovered');
+  }
+}
+
+function updateStars (element, className) {
+  var parent = element.parentNode;
+  var allStars = parent.querySelectorAll('.stars a');
+  var allStarsArr = Array.prototype.slice.call(allStars);
+  var currentElementPosition = allStarsArr.indexOf(element);
+  var i = 0;
+
+  for (i = 0; i < allStarsArr.length; i++) {
+    allStarsArr[i].classList.remove('full');
+
+    if (i <= currentElementPosition) {
+      allStarsArr[i].classList.add(className);
+    } else {
+      allStarsArr[i].classList.remove(className);
+    }
+  }
+}
+
+// switching tabs
